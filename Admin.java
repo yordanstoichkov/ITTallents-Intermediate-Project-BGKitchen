dec0a2159c6970b3cabd9fb321bac0b541bc93ad
dakata5 <slav4o95@gmail.com>
2016-08-18 21:21:54 +0300

    Deleting useless files!

--- Admin.java
+++
@@ -1,9 +0,0 @@
-package Project.start;
-
-public class Admin extends User{
-
-	public Admin(String userName, String password, String email) {
-		super(userName, password, email);
-	}
-
-}
--- Menu.java
+++
@@ -1,5 +0,0 @@
-package Project.start;
-
-public class Menu {
-
-}
